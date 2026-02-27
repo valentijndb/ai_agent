e987214e08506c97652049ad7152ecf082157c66
valentijndb <valentijndb@gmail.com>
2026-02-27 21:52:11 +0100

    output

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+def main():
+
+    parser = argparse.ArgumentParser(description="Chatbox")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])] 
+    
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key == None:
+        raise RuntimeError("Api key was not found")
+    
+    
+    
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents= messages)
+
+    if response.usage_metadata == None:
+        raise RuntimeError("failed API request")
+    
+    if args.verbose:    
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Response:")
+        print(response.text)
+    else:
+        print(f"Response:")
+        print(response.text)
 
 if __name__ == "__main__":
     main()
