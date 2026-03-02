e3262153ba410efd0975bcc64c4a46fb2117bafa
valentijndb <valentijndb@gmail.com>
2026-03-02 09:17:57 +0100

    systemprompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 def main():
 
@@ -22,7 +23,11 @@ def main():
     
     
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash", contents= messages)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents= messages, 
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        )
 
     if response.usage_metadata == None:
         raise RuntimeError("failed API request")
