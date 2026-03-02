789b46485463ffdc5c1b771110c88534daf920b9
valentijndb <valentijndb@gmail.com>
2026-03-02 10:00:57 +0100

    callfunction

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from call_function import available_functions
 
 def main():
 
@@ -26,7 +27,7 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.5-flash", 
         contents= messages, 
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
         )
 
     if response.usage_metadata == None:
