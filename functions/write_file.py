58c2a94078cafdacc5c9c296714ce7917b413497
valentijndb <valentijndb@gmail.com>
2026-03-03 09:02:00 +0100

    write file updated

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,39 +1,41 @@
 import os
+
 from google.genai import types
+
+
+def write_file(working_directory, file_path, content):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if os.path.isdir(abs_file_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+    except Exception as e:
+        return f"Error: writing to file: {e}"
+
 
 schema_write_file = types.FunctionDeclaration(
     name="write_file",
-    description="Write or overwrite files",
+    description="Writes text content to a specified file within the working directory (overwriting if the file exists)",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
             "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="relative file_path of file",
+                description="Path to the file to write, relative to the working directory",
             ),
             "content": types.Schema(
                 type=types.Type.STRING,
-                description="content to be written",
-            )
+                description="Text content to write to the file",
+            ),
         },
-        required=["file_path","content"]
+        required=["file_path", "content"],
     ),
 )
-
-def write_file(working_directory, file_path, content):
-  try:
-      abs_working_dir = os.path.abspath(working_directory)
-      target_dir = os.path.normpath(os.path.join(abs_working_dir, file_path))
-      if os.path.commonpath([abs_working_dir, target_dir]) != abs_working_dir:
-        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
-      if os.path.isdir(target_dir):
-        return f'Error: "Cannot write to "{file_path}" as it is a directory'
-      
-      os.makedirs(file_path, exist_ok=True)
-      with open(target_dir,"w") as file:
-         file.write(content)
-      
-      return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
-     
-  except Exception as e:
-      return f"Error: {e}"
